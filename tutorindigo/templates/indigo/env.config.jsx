6119ad6817aa1339b8fc020f8fe6cb41d64a02fc
ibrahdz <ibraim@tdrobotica.co>
2024-09-27 18:55:57 -0500

    Update env.config.jsx

--- tutorindigo/templates/indigo/env.config.jsx
+++ tutorindigo/templates/indigo/env.config.jsx
@@ -40,7 +40,7 @@ const config = {
             id: 'custom_footer',
             type: DIRECT_PLUGIN,
             RenderWidget: () => (
-              <h1 style={{textAlign: 'center'}}>🦶</h1>
+              <h1 style={{textAlign: 'center'}}>Esta es una prueba de footer persoanlizado</h1>
             ),
           },
         },
