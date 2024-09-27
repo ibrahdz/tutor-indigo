0c3fe18ed789fb2cc60d18b36965339177930c0c
ibrahdz <ibraim@tdrobotica.co>
2024-09-27 09:11:22 -0500

    Update plugin.py

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -19,20 +19,18 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
     # Add here your new settings
     "defaults": {
         "VERSION": __version__,
-        "WELCOME_MESSAGE": "The place for all your online learning",
+        "WELCOME_MESSAGE": "La academia de tdrobotica",
         "ENABLE_DARK_THEME": False,
-        "PRIMARY_COLOR": "#15376D",  # Indigo
+        "PRIMARY_COLOR": "#E30613",  # Indigo
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[]
         "FOOTER_NAV_LINKS": [
-            {"title": "About Us", "url": "/about"},
-            {"title": "Blog", "url": "/blog"},
-            {"title": "Donate", "url": "/donate"},
-            {"title": "Terms of Service", "url": "/tos"},
-            {"title": "Privacy Policy", "url": "/privacy"},
-            {"title": "Help", "url": "/help"},
-            {"title": "Contact Us", "url": "/contact"},
+            {'title': 'Nosotros', 'url': 'https://tdrobotica.co/nosotros'},
+            {'title': 'Explorar', 'url': 'https://tdrobotica.co/'},
+            {'title': 'Comprar', 'url': 'https://tdrobotica.co/tienda'},
+            {'title': 'Aprender', 'url': 'https://aprender.tdrobotica.co'},
+            {'title': 'Contactanos', 'url': 'https://tdrobotica.co/#contact'}
         ],
     },
     "unique": {},
