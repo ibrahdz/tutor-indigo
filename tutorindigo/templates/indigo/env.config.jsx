b62be559202f5fe4ffecbbf8c9ceacc1d87e5959
ibrahdz <ibraim@tdrobotica.co>
2024-09-27 19:37:48 -0500

    Update env.config.jsx

--- tutorindigo/templates/indigo/env.config.jsx
+++ tutorindigo/templates/indigo/env.config.jsx
@@ -20,40 +20,7 @@ const config = {
             type: DIRECT_PLUGIN,
             RenderWidget: () => (
               <div class="wrapper wrapper-footer">
-                <footer id="footer" class="tutor-container">
-                <div class="footer-top">
-                  <div class="powered-area">
-                    <ul class="logo-list">
-                      <li>Por:</li>
-                      <li>
-                        <a href="https://www.tdrobotica.co" rel="noopener" target="_blank">
-                          <img src="${static.url('https://academia.tdrobotica.co/theming/asset/images/tutor-logo.png')}" alt="Por tdrobotica" width="79" />
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <nav class="nav-colophon" aria-label="${_('About')}">
-                    <ol>
-                      <li>
-                        <a href="https://tdrobotica.co/nosotros">Nosotros</a>
-                      </li>                        
-                      <li>
-                        <a href="https://tdrobotica.co/">Explorar</a>
-                      </li>                        
-                      <li>
-                        <a href="https://tdrobotica.co/tienda">Comprar</a>
-                      </li>                        
-                      <li>
-                        <a href="https://aprender.tdrobotica.co">Aprender</a>
-                      </li>                        
-                      <li>
-                        <a href="https://tdrobotica.co/#contact">Contactanos</a>
-                      </li>                        
-                    </ol>
-                  </nav>
-                </div>
-                <span class="copyright-site">Copyrights ©${datetime.now().year}. Todos los derechos reservados.</span>                
-                </footer>
+                <span>prueba de footer</span>
               </div>            
             ),
           },
