0dd1794ef3f351463d5c602e1e01e4218baf1ff5
ibrahdz <ibraim@tdrobotica.co>
2024-09-27 19:22:48 -0500

    Update env.config.jsx

--- tutorindigo/templates/indigo/env.config.jsx
+++ tutorindigo/templates/indigo/env.config.jsx
@@ -19,7 +19,23 @@ const config = {
             id: 'custom_footer',
             type: DIRECT_PLUGIN,
             RenderWidget: () => (
-              <h1>Esta es una prueba de footer persoanlizado</h1>
+              <div class="wrapper wrapper-footer">
+                <footer id="footer" class="tutor-container">
+                <div class="footer-top">
+                  <div class="powered-area">
+                    <ul class="logo-list">
+                      <li>Por:</li>
+                      <li>
+                        <a href="https://www.tdrobotica.co" rel="noopener" target="_blank">
+                          <img src="${static.url('images/logo.png')}" alt="Por tdrobotica" width="79" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <span class="copyright-site">Copyrights ©${datetime.now().year}. Todos los derechos reservados.</span>                
+                </footer>
+              </div>            
             ),
           },
         },
